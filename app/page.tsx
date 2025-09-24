e952772cba9f2261d061aa2bf938be090dd38e60
Alexander Pleshakov <sh.plh@yandex.ru>
2025-09-25 00:26:10 +0300

    add temp controls

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,108 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import { Synth } from "./synth";
+import { MouseTracker, Position } from "./mouse-track";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const context = new AudioContext();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  let oscillator: OscillatorNode | null = null;
+
+  let gain: GainNode | null = null;
+
+  let currentFrequency: number | null = null;
+
+  const setupOscillator = () => {
+    oscillator = context.createOscillator();
+    gain = context.createGain();
+
+    oscillator.type = "sine";
+    oscillator.connect(gain);
+    gain.connect(context.destination);
+
+    oscillator.start();
+  };
+
+  const closestNoteFrequency = (frequency: number): number => {
+    const midi = Math.round(69 + 12 * Math.log2(frequency / 440));
+    return 440 * Math.pow(2, (midi - 69) / 12);
+  };
+
+  const clickNote = (frequency: number) => {
+    const now = context.currentTime;
+
+    if (oscillator === null) {
+      setupOscillator();
+    }
+
+    if (!oscillator) {
+      return;
+    }
+
+    if (!gain) {
+      return;
+    }
+
+    // Cancel any previous automation to avoid conflicts
+    oscillator.frequency.cancelScheduledValues(now);
+    gain.gain.cancelScheduledValues(now);
+
+    // If same note is playing, fade it out first
+    if (frequency === currentFrequency) {
+      gain.gain.cancelScheduledValues(now);
+      // gain.gain.setValueAtTime(0, now + 0.01);
+      gain.gain.linearRampToValueAtTime(0, now + 0.05);
+
+      gain.gain.cancelScheduledValues(context.currentTime + 0.1);
+      // Make sure volume is up
+      // gain?.gain.setValueAtTime(gain.gain.value, now);
+      gain.gain.linearRampToValueAtTime(1, context.currentTime + 0.11);
+
+      return;
+    }
+
+    currentFrequency = frequency;
+
+    // Make sure volume is up
+    gain.gain.setValueAtTime(gain.gain.value, now);
+    gain.gain.linearRampToValueAtTime(1, now + 0.01);
+
+    // Smooth glide in 0.3 seconds (tweak this time for faster/slower glide)
+    oscillator.frequency.linearRampToValueAtTime(frequency, now + 0.3);
+  };
+
+  const stopNote = () => {
+    if (!gain) return;
+
+    const now = context.currentTime;
+
+    // Fade out over 0.3s
+    gain.gain.cancelScheduledValues(now);
+    gain.gain.setValueAtTime(gain.gain.value, now);
+    gain.gain.linearRampToValueAtTime(0, now + 0.3);
+  };
+
+  const trackpadPositionChanged = (position: Position) => {
+    const frequency = closestNoteFrequency(position.x + 100);
+
+    clickNote(frequency);
+  };
+
+  return (
+    <div className="h-full flex justify-center content-center gap-2">
+      <MouseTracker positionChanged={trackpadPositionChanged} />
+      <button className="bg-amber-300" onClick={() => clickNote(130.8)}>
+        130.8
+      </button>
+      <button className="bg-amber-300" onClick={() => clickNote(174.6)}>
+        174.6
+      </button>
+      <button className="bg-amber-300" onClick={() => clickNote(329.6)}>
+        329.6
+      </button>
+      <button className="bg-amber-300" onClick={() => stopNote()}>
+        stop
+      </button>
     </div>
   );
 }
